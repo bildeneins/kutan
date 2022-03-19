da24b3b521e68ec8af0d5b5edc5b3df612023c63
Yajima Soichi <soichi513@icloud.com>
2022-03-19 17:33:14 +0900

    前方一致検索と開講部局の絞り込みを実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,116 @@
-import type { NextPage } from 'next'
-import { Button, Page, Text, Tabs, useTheme, Divider } from '@geist-ui/core'
-import Head from 'next/head'
+import type { GetServerSideProps, NextPage } from 'next'
+import { Button, Card, Grid, Input, Page, Pagination, Select, Spacer, Table, Text, useInput } from '@geist-ui/core'
+import prisma from '../lib/prisma'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import {ChevronLeft, ChevronRight, Search} from '@geist-ui/icons'
 
-const Home: NextPage = () => {
-  const { palette } = useTheme()
+interface SubjectWhereInput {
+  name?: {
+    startsWith: string
+  },
+  faculty?: {
+    id: number
+  }
+}
+
+export const getServerSideProps
+    : GetServerSideProps<{
+        subjects: any,
+        counts: number,
+        faculties: any[]
+      }>
+    = async ({ query }) => {
+  const skip = (query.skip !== undefined) ? parseInt(query.skip as string) : 0
+  const where: SubjectWhereInput = {
+    name: query.searchQuery ? {
+      startsWith: query.searchQuery as string
+    } : undefined,
+    faculty: query.faculty != undefined && query.faculty != '' ? {
+      id: parseInt(query.faculty as string)
+    } : undefined
+  }
+  const subjects = await prisma.subject.findMany({
+    skip,
+    take: 10,
+    include: {
+      faculty: true
+    },
+    where
+  })
+  const counts = await prisma.subject.count({
+    where
+  })
+  const faculties = await prisma.faculty.findMany()
+  return {
+    props: {
+      subjects,
+      counts,
+      faculties
+    }
+  }
+}
+
+const Home: NextPage<{ subjects: any, counts: number, faculties: any[] }> = ({ subjects, counts, faculties }: { subjects: any, counts: number, faculties: any[] }) => {
+  const router = useRouter()
+  const [skipNum, setSkipNum] = useState(0)
+  const [faculty, setFaculty] = useState<undefined | { id: number, name: string }>(undefined)
+  const {
+    state: searchQuery,
+    bindings
+  } = useInput('')
+
+  const paginationNum = Math.ceil(counts / 10)
+
+  const formattedSubjects = subjects.map((i: any) => {
+    return {
+      id: i.id,
+      name: i.name,
+      facultyName: i.faculty.name,
+      percentage: Math.floor(1000 * i.earnSum / i.registerSum) / 10
+    }
+  })
+
+  const pushRoute = async ({
+      newFaculty,
+      newSearchQuery,
+      newSkipNum
+                           }: {
+    newFaculty?: { id: number, name: string } | undefined,
+    newSearchQuery?: string,
+    newSkipNum?: number
+  }) => {
+    await router.push({
+      pathname: "/",
+      query: {
+        skip: newSkipNum ? newSkipNum : skipNum,
+        searchQuery: newSearchQuery ? newSearchQuery : searchQuery,
+        faculty: newFaculty ? newFaculty.id
+                            : faculty ? faculty.id : undefined
+      }
+    })
+  }
+
+  const search = async () => {
+    const newSkipNum = 0
+    setSkipNum(newSkipNum)
+    await pushRoute({ newSkipNum })
+  }
+
+  const onChangePage = async (val: number) => {
+    const newSkipNum = 10 * (val - 1)
+    setSkipNum(newSkipNum)
+    await pushRoute({ newSkipNum })
+  }
+
+  // val: 'all' | '0' | '1' | ...
+  const handleSelect = (val: string | string[]) => {
+    const faculty: { id: number, name: string } | undefined = val === 'all' ? undefined
+                      : faculties.find((f: any) => f.id.toString() === val)
+    setFaculty(faculty)
+    setSkipNum(0)
+    pushRoute({ newFaculty: faculty ? faculty : undefined })
+  }
 
   return (
     <Page>
@@ -13,7 +120,71 @@ const Home: NextPage = () => {
         </div>
       </Page.Header>
       <Page.Content>
-
+        <Grid.Container>
+          <Grid xs={4}><Input
+              {...bindings}
+          /></Grid>
+          <Spacer w={1}/>
+          <Grid xs={4}>
+            <Button
+              onClick={search}
+              scale={3/4}
+              icon={<Search />}
+              auto
+            >
+              Search
+            </Button>
+          </Grid>
+        </Grid.Container>
+        <Spacer h={1} />
+        <Select
+            placeholder={'開講部局'}
+            onChange={handleSelect}
+        >
+          <Select.Option value={'all'}>全て</Select.Option>
+          {
+            faculties.map((faculty: any) => {
+              return (
+                  <Select.Option
+                      value={faculty.id.toString()}
+                      key={faculty.id}
+                  >
+                    {faculty.name}
+                  </Select.Option>
+              )
+            })
+          }
+        </Select>
+        <Text p>
+          {counts} 件中{formattedSubjects.length}件を表示
+        </Text>
+        <Card>
+          <Table data={formattedSubjects}>
+            <Table.Column prop="name" label="講義名" />
+            <Table.Column prop="facultyName" label="開講部局" />
+            <Table.Column prop="percentage" label="単位取得率"
+                          render={(value: any) => {
+                            return (<>{value.toString()}%</>)
+                          }}
+            />
+          </Table>
+          <Spacer h={1}/>
+          <Grid.Container justify="center">
+            <Grid>
+              <Pagination
+                  count={paginationNum}
+                  onChange={onChangePage}
+              >
+                <Pagination.Next>
+                  <ChevronRight />
+                </Pagination.Next>
+                <Pagination.Previous>
+                  <ChevronLeft />
+                </Pagination.Previous>
+              </Pagination>
+            </Grid>
+          </Grid.Container>
+        </Card>
       </Page.Content>
       <Page.Footer>
         <Text p>2022</Text>
